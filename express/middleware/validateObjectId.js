42184347c2b3a0fb483456680b342c427d3ffe03
Ian Badino <ianlucab@hotmail.com.ar>
2025-10-30 09:19:53 -0300

    fix middleware para validar ids

--- express/middleware/validateObjectId.js
+++ express/middleware/validateObjectId.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const validateObjectId = (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     return res.status(400).json({ message: 'ID no válido' });
